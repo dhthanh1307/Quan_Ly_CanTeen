f5b26a76a0f90d9aa3739f9ef1e70befa6192a8f
21120134 <117473361+dhthanh1307@users.noreply.github.com>
2023-11-08 16:58:02 +0700

    chia component

---
+++ trang_chu.js
@@ -0,0 +1,37 @@
+export default{
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    },
+    template:`
+    <div class="page_heading">
+            <div class="quan_ly_can_tin_txt">Quản lý căn tin</div>
+            <div class="divider"></div>
+          </div>
+    <div class="frame2">
+        <div class="frame3">
+        <div class="bao_cao_doanh_thu_container" id="containerBaoCaoDoanhThu">
+            <div class="bao_cao_doanh_thu_txt">Báo cáo doanh thu</div>
+        </div>
+        <summary class="quan_ly_kho_hang_container" id="containerQuanLyKhoHang">
+            <div class="quan_ly_kho_hang_txt">Quản lý kho hàng</div>
+        </summary>
+        </div>
+    </div>
+    <div class="frame4">
+        <div class="frame3">
+        <div class="quan_ly_thuc_don_container" id="containerQuanLyThucDon">
+            <div class="quan_ly_thuc_don_txt">Quản lý thực đơn</div>
+        </div>
+        <div class="ban_hang_container" id="containerBanHang">
+            <div class="frame6">
+            <div class="quan_ly_ban_hang_txt">Bán hàng</div>
+            </div>
+        </div>
+        </div>
+    </div>`
+}
